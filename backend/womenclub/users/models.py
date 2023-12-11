bfb07773fcd4a22ec415e476eca23a6ca190cd0c
Мальцева Любовь <rikyhollundead1234@gmail.com>
2023-12-11 18:36:16 +0500

    Создание лк

--- backend/womenclub/users/models.py
+++ backend/womenclub/users/models.py
@@ -1,5 +1,15 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
-# Create your models here.
 class User(AbstractUser):
 	pass
+
+class Schedule(models.Model):
+	subject = models.CharField(max_length=100)
+	date = models.DateField()
+	time = models.TimeField()
+	def __str__(self):
+		return f"{self.date} - {self.time.strftime('%d-%m-%Y')} {self.subject}"
+
+	class Meta:
+		verbose_name_plural = 'Расписание'
--- backend/womenclub/womenclub/urls.py
+++ backend/womenclub/womenclub/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
 	path('', views.index, name='index'),
     path('users/', include('users.urls')),
+     path('admin/', admin.site.urls),
 ]
 
